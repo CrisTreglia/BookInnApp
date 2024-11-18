0ea10dfb81dbbb4f5a6304b6d3cf74111cde821e
Cristofer Treglia <cristofertreglia@outlook.com>
2024-11-18 09:44:09 -0700

    Creating Hotel Form and components

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -21,7 +21,12 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-//app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || 'https://bookinnapp.onrender.com/',
+    credentials: true,
+  })
+);
 
 //Serving static assets
 app.use(express.static(path.join(__dirname, "../../frontend/dist")));
--- frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
+++ frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
@@ -1,4 +1,5 @@
 import { FormProvider, useForm } from "react-hook-form";
+import DetailsSection from "./DetailsSection";
 
 export type HotelFormData = {
     name: string;
@@ -19,7 +20,7 @@ const ManageHotelForm = () => {
     return (
         <FormProvider { ...formMethods}>
             <form>
-                
+                <DetailsSection/>
             </form>
         </FormProvider>
     );
